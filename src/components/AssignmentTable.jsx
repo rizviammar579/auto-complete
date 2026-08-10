a42558fbf2ad0c33417bfbe27e4441466f1b6cad
Ammar Rizvi <rizviammar579@gmail.com>
2026-08-10 22:32:03 +0530

    started with assignments page

--- src/components/AssignmentTable.jsx
+++ src/components/AssignmentTable.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const AssignmentTable = () => {
 
+  
     const assignments = [
     { id: 1, course: 'Data Structures', assignment: "Linked List", dueDate: 'due in 3 hours', AIStatus: 'processed', driveLink: "dvekvhfivljkhkk438r8g", submissionStatus: 'turned in' },
     { id: 2, course: 'Data Structures', assignment: "Linked List", dueDate: 'due in 3 hours', AIStatus: 'processed', driveLink: "dvekvhfivljkhkk438r8g", submissionStatus: 'turned in' },
@@ -23,36 +24,19 @@ const AssignmentTable = () => {
           <th className="px-6 py-4 text-left font-semibold">Assignment</th>
           <th className="px-6 py-4 text-left font-semibold">Due Date</th>
           <th className="px-6 py-4 text-left font-semibold">AI Status</th>
-          <th className="px-6 py-4 text-left font-semibold">Drive Link</th>
-          <th className="px-6 py-4 text-left font-semibold">Submission Status</th>
         </tr>
       </thead>
 
       <tbody>
 
-          <tr className="border-b border-gray-200 hover:bg-gray-50 transition">
-            <td className="px-6 py-4 ">Data Structure</td>
-            <td className="px-6 py-4 ">Linked List</td>
-            <td className="px-6 py-4 ">due in 3 hours</td>
-            <td className="px-6 py-4 text-blue-800 font-bold">generating</td>
-            <td className="px-6 py-4 ">dvekvhfivljkhkk438r8g</td>
-            <td className="px-6 py-4 ">
-                <button className='bg-blue-700 text-gray-100 px-3 py-1 w-fit rounded-xl'>Mark as turned in</button>
-            </td>
           
-          </tr>
         {assignments.map((assignment) => (
           <tr key={assignment.id} className="border-b border-gray-200 hover:bg-gray-50 transition">
             <td className="px-6 py-4 ">{assignment.course}</td>
             <td className="px-6 py-4 ">{assignment.assignment}</td>
             <td className="px-6 py-4 ">{assignment.dueDate}</td>
             <td className="px-6 py-4 text-green-500 font-bold">{assignment.AIStatus}</td>
-            <td className="px-6 py-4 ">{assignment.driveLink}</td>
-            <td className="px-6 py-4 ">
-                <div className='border border-[1px] px-3 w-fit rounded-full bg-green-300'>
-                    {assignment.submissionStatus}
-                </div>
-            </td>
+        
           
           </tr>
         ))}
--- src/pages/AssignmentsPage.jsx
+++ src/pages/AssignmentsPage.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
 import AssignmentTable from '../components/AssignmentTable'
+import { useState } from 'react'
+import { Loader } from '../components/Loader'
 
 const AssignmentsPage = () => {
+
+   
+  const [assignmentData, setAssignmentData] = useState([])
+  const [currentFilter, setCurrentFilter] = useState('All')
+
+  const filters = [
+    { text: 'All' },
+    { text: 'Submitted' },
+    { text: 'Unsubmitted' },
+    { text: 'Pending' },
+    { text: 'Generated' },
+    { text: 'Manual Review' },
+  ]
+
+
+  if (!assignmentData) {
+        return <Loader />
+      }
+
   return (
-    <div className='font-inter p-5 flex flex-col gap-7 overflow-x-auto'>
-    <h1 className='text-[28px] font-bold'>Assignment Overview</h1>
-    <AssignmentTable/>
+    <div className='font-inter flex gap-5 overflow-x-auto'>
+
+
+      <div className='w-[60%] flex flex-col gap-10 m-5'>
+
+        <div className='border border-gray-300 w-fit rounded-[8px] border-[2px] mt-5'>
+
+          {filters.map(filter => {
+
+            return <button key={filter.text} className={`text-[14px]  w-fit rounded-[8px] py-1 px-3 cursor-pointer ${filter.text === currentFilter ? 'bg-gray-800 text-gray-100 ' : 'text-gray-800 bg-gray-100 font-semibold'}`} onClick={() => setCurrentFilter(filter.text)}>{filter.text}</button>
+
+          })}
+
+
+        </div>
+
+        <AssignmentTable />
+
+      </div>
+
+
+
+      <div className='border w-[40%] '>hi</div>
+
+
+
     </div>
   )
 }
