45c8e61fab565147e7be043b53f3ab3bc555442c
Ammar Rizvi <rizviammar579@gmail.com>
2026-08-12 00:03:09 +0530

    changed empty state in assignments page

--- src/components/NoAssignments.jsx
+++ src/components/NoAssignments.jsx
@@ -1,9 +1,12 @@
+import { CheckCircle2 } from 'lucide-react'
 import React from 'react'
+
 
 const NoAssignments = () => {
   return (
-    <div className='w-[500px] py-20 relative top-0 right-[100%] text-red-500 font-semibold bg-red-100  rounded-xl border border-red-300 text-2xl flex justify-center items-center'>
-      There are no such assignments ! 
+    <div className='w-[500px] py-20 relative top-0 right-[100%] text-xl text-gray-500 flex justify-center items-center gap-2'>
+      <CheckCircle2/>
+      You're all caught up! 
     </div>
   )
 }
