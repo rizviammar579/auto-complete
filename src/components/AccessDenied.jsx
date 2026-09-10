43eee79ff3d05c53e30d1e5a93f59557f05f40c9
Ammar Rizvi <rizviammar579@gmail.com>
2026-09-10 13:05:23 +0530

    changed UI for action restricted  notification

--- src/components/AccessDenied.jsx
+++ src/components/AccessDenied.jsx
@@ -1,7 +1,7 @@
 import { useAccessDenied } from "../context/AccessDeniedContext.jsx";
 
 function AccessDenied() {
-    
+
     const { show } = useAccessDenied();
 
     if (!show) {
@@ -10,13 +10,16 @@ function AccessDenied() {
 
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/20 backdrop-blur-sm">
-            <div className="rounded-xl border border-gray-200 bg-white px-10 py-8 text-center shadow-xl">
-                <h2 className="text-xl font-bold text-gray-900">
-                    Access Restricted
+            <div className="flex flex-col gap-3 rounded-xl border border-gray-200 bg-white px-10 py-8 text-center shadow-xl w-xl">
+                <h2 className="text-4xl font-bold text-red-500 flex justify-center items-center gap-2">
+                    <img src="../access-denied.png" alt="" className="w-[50px]" /> Action Restricted!
                 </h2>
 
-                <p className="mt-2 text-sm text-gray-600">
+                <p className="mt-2 text-xl text-gray-800">
                     You don't have permission to perform this action.
+                </p>
+                <p className="mt-2 text-sm text-gray-600 mt-8">
+                    Your account has view-only access to Auto-Complete. Actions that modify assignments or data are restricted to authorized accounts.
                 </p>
             </div>
         </div>
