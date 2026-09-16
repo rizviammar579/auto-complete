cac4b88232fbc16e4a64936ec8becc839b1979d0
Ammar Rizvi <rizviammar579@gmail.com>
2026-09-16 23:08:42 +0530

    fix/custom 404 page responsiveness

--- src/components/Custom404.jsx
+++ src/components/Custom404.jsx
@@ -8,10 +8,12 @@ const Custom404 = () => {
     return (
         <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh] font-inter gap-5'>
 
-            <div className='font-bold text-9xl'>404</div>
-            <div className='font-light text-4xl'>Page Not Found</div>
-            <p>We couldn't find the page you were looking for. It may have been moved, renamed or doesn't exist.</p>
-            <button className='bg-blue-500 text-white cursor-pointer rounded-4xl p-5 mt-7' onClick={() => { navigate('/dashboard') }}>Return to Dashboard</button>
+
+            <div className='font-bold text-7xl cx:text-8xl'>404</div>
+            <div className='font-light text-3xl cx:text-4xl'>Page Not Found</div>
+            <p className='w-[70%] text-center'>We couldn't find the page you were looking for. It may have been moved, renamed or doesn't exist.</p>
+            <button className='bg-blue-500 text-white cursor-pointer rounded-4xl p-3 cx:p-5 mt-7' onClick={() => { navigate('/dashboard') }}>Return to Dashboard</button>
+
 
         </div>
     )
